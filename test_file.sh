1ee08fecaded1a9d86e0cf075c95737865ead82b
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-09-07 20:19:57 +0200

    Update test_file.sh

--- test_file.sh
+++ test_file.sh
@@ -13,17 +13,14 @@
 # @section Example functions
 
 # @description My super function.
-# Not thread-safe.
 #
 # @example
 #    echo "test: $(say-hello World)"
 #
-# @arg $1 string A value to print
+# @arg $1 integer A value to print
 #
 # @exitcode 0 If successful.
 # @exitcode 1 If an empty string passed.
-#
-# @see validate()
 say-hello() {
     if [[ ! "$1" ]]; then
         return 1;
